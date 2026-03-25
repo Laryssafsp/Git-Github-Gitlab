f6cacce529d5cb74ee1b2bd0aaaf55c861786b0d
Laryssa <88940724+Laryssafsp@users.noreply.github.com>
2026-03-25 13:41:38 -0300

    Create concat_dados.py

---
+++ DIO/formacao-github-certification/concat_dados.py
@@ -0,0 +1,12 @@
+# Descrição: Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+# Entrada: O usuário irá digitar dois dados diferentes, por exemplo: "Olá" e "Mundo"
+# Saída: O programa irá concatenar os dois dados e exibir a mensagem "Olá Mundo"
+
+info1 = input("Digite o primeiro dado: ")
+info2 = input("Digite o segundo dado: ")
+resultado = info1 + " " + info2
+print("O resultado da concatenação é:", resultado)
+
+
+# como executar o código: Salve o código em um arquivo chamado "concat_dados.py" e execute-o usando o comando "python concat_dados.py" no terminal. O programa irá solicitar que você digite dois dados, e após a entrada, ele exibirá a mensagem concatenada.
+print("O resultado da concatenação é:", resultado)
